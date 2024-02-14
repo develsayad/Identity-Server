4036c7a970e543fedfb3e138fed250216551e320
MohamedElasyad <melasyad@snih.gov.sa>
2024-02-14 10:16:41 +0300

    adding ui to identity server.

--- Core.IdentitySvr/Program.cs
+++ Core.IdentitySvr/Program.cs
@@ -6,12 +6,7 @@ using IdentityServer4.Test;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-//builder.Services.AddIdentityServer()
-//    .AddDeveloperSigningCredential()
-//    .AddInMemoryApiResources(Config.GetAllApiResources())
-//    .AddInMemoryClients(Config.GetClients());
-
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddIdentityServer()
     .AddInMemoryClients(Config.Clients)
@@ -24,17 +19,17 @@ builder.Services.AddIdentityServer()
 
 
 var app = builder.Build();
+
+app.UseStaticFiles();
 app.UseRouting();
 app.UseIdentityServer();
+app.UseAuthorization();
 
 
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapGet("/", async context =>
-//    {
-//        await context.Response.WriteAsync("hello world");
-//    });
-//});
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 app.Run();
