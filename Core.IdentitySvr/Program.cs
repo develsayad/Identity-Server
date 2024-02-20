77ae31d8707cb0f38444eb4337534ef224e1d95b
MohamedElasyad <melasyad@snih.gov.sa>
2024-02-20 07:26:39 +0300

    start adding asp.net core identity.

--- Core.IdentitySvr/Program.cs
+++ Core.IdentitySvr/Program.cs
@@ -1,20 +1,48 @@
 
 
 using Core.IdentitySvr;
+using Core.IdentitySvr.Models;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Internal;
+using System.Reflection;
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var migrationsAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+const string connectionString = @"Data Source=DESKTOP-APPM9TF;database=IdentityServer4.Quickstart.EntityFramework-4.0.0;trusted_connection=yes;";
+
+//builder.Services.AddIdentity<ApplicationUser,IdentityRole>()
+//    .AddEntityFrameworkStores< ApplicationDbContext>
+
 
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddIdentityServer()
-    .AddInMemoryClients(Config.Clients)
-    .AddInMemoryIdentityResources(Config.IdentityResources)
-    //.AddInMemoryApiResources(Config.ApiResources)
-    .AddInMemoryApiScopes(Config.ApiScopes)
     .AddTestUsers(Config.TestUsers)
-    .AddDeveloperSigningCredential();
+    .AddConfigurationStore(options => {
+        options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+               sql => sql.MigrationsAssembly(migrationsAssembly));
+    })
+     .AddOperationalStore(options =>
+     {
+         options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+             sql => sql.MigrationsAssembly(migrationsAssembly));
+     });
+
+
+//.AddInMemoryClients(Config.Clients)
+//.AddInMemoryIdentityResources(Config.IdentityResources)
+////.AddInMemoryApiResources(Config.ApiResources)
+//.AddInMemoryApiScopes(Config.ApiScopes)
+
+//.AddDeveloperSigningCredential();
 
 
 
